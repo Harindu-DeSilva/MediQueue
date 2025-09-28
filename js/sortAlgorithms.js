24e8c24bf9dca8203b6aa7410812e2f5adc6c629
Induwara Bhagya <Induwarabhagya1210@gmail.com>
2025-09-28 07:46:55 +0530

    add insertion sort algorithm

---
+++ js/sortAlgorithms.js
@@ -0,0 +1,17 @@
+function insertionSort(arr, key) {
+	for (let i = 1; i < arr.length; i++) {
+		let current = arr[i];
+		let j = i - 1;
+		while (j >= 0 && arr[j][key] > current[key]) {
+			arr[j + 1] = arr[j];
+			j--;
+		}
+		arr[j + 1] = current;
+	}
+}
+
+
+const sortingAlgorithms = {
+	insertion: insertionSort
+};
+window.sortingAlgorithms = sortingAlgorithms;
